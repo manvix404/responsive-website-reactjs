f496181ae9fffb48d614b221cce64279cac39452
manvik S <manvik55@gmail.com>
2022-05-20 11:14:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {Footer, Blog, Possibility, Feature,WhatGPT3,Header} from './containers'
+import {Navbar,Brand,CTA} from './Components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Feature/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Feature from '../feature/Feature'
+import './whatGPT3.css'
+function WhatGPT3() {
+  return (
+    <div className='gpt3__whatgpt3 section__margin' id='whgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature/>
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>The Possibilities are beyond your imagination</h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature/>
+        <Feature/>
+        <Feature/>
+      </div>
+    </div>
+  )
+}
+
+export default WhatGPT3
