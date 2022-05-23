2448ed24392f1c88c445f0326418f4aacf7c6985
manvik S <manvik55@gmail.com>
2022-05-23 11:39:13 +0530

    added cta

--- src/Components/cta/CTA.jsx
+++ src/Components/cta/CTA.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import './cta.css'
 function CTA() {
   return (
-    <div>CTA</div>
+    <div className='gpt3__cta'>
+      <div className='gpt3__cta-content'>
+        <p>Request Early Access</p>
+        <h3>Register today and start exploring the endless possibility</h3>
+      </div>
+      <div className='gpt3__cta-btn'>
+        <button type='button'>Get started</button>
+      </div>
+    </div>
   )
 }
 
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import possibilityImage from '../../assets/possibility.png'
 import './possibility.css'
 function Possibility() {
   return (
-    <div>Possibility</div>
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt=""/>
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Request Early Access to get start</h4>
+        <h1 className='gradient__text'>The Possibility is beyond your imagination</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h4>Request early access to get started</h4>
+      </div>
+    </div>
   )
 }
 
